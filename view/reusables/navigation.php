3422ffe893b31f802ef8a0ac155733f830345d41
oussama <oussama.ennadafy@gmail.com>
2022-05-12 22:44:43 +0100

    update

--- view/reusables/navigation.php
+++ view/reusables/navigation.php
@@ -5,13 +5,13 @@
       <a class="px-3 py-2 rounded-md hover:bg-gray-100" href="http://localhost/fill-rouge/user/nav/OurSports">
         Our sports
       </a>
-      <a class="px-3 py-2 rounded-md hover:bg-gray-100" href="http://localhost/fill-rouge/user/nav/men’s">
+      <a class="px-3 py-2 rounded-md hover:bg-gray-100" href="http://localhost/fill-rouge/user/nav/mens">
         Men&#8217;s
       </a>
-      <a class="px-3 py-2 rounded-md hover:bg-gray-100" href="http://localhost/fill-rouge/user/nav/women’s">
+      <a class="px-3 py-2 rounded-md hover:bg-gray-100" href="http://localhost/fill-rouge/user/nav/womens">
         Women&#8217;s
       </a>
-      <a class="px-3 py-2 rounded-md hover:bg-gray-100" href="http://localhost/fill-rouge/user/nav/kid’s">
+      <a class="px-3 py-2 rounded-md hover:bg-gray-100" href="http://localhost/fill-rouge/user/nav/kids">
         Kid&#8217;s
       </a>
       <a class="px-3 py-2 rounded-md hover:bg-gray-100" href="http://localhost/fill-rouge/user/nav/accessoires">
