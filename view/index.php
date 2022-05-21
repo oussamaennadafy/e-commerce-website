61629626388babe803edc0e835371d6ba9033fb3
oussama <oussama.ennadafy@gmail.com>
2022-05-21 16:51:53 +0100

    update

--- view/index.php
+++ view/index.php
@@ -180,7 +180,7 @@
                 <div>
                   <a
                     class="px-4 py-2 inline-block text-white text-center bg-blue-600 border border-transparent rounded-md hover:bg-blue-700 select-none"
-                    href="#"
+                    href="http://localhost/fill-rouge/user/details/<?php echo $LastProduct['id'] ?>"
                   >
                     Add to cart
                   </a>
