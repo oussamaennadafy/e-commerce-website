a1cb1435f27abf747fc45e930108539eb948e819
oussama <oussama.ennadafy@gmail.com>
2022-06-13 20:54:02 +0100

    update

--- controller/UserController.php
+++ controller/UserController.php
@@ -669,9 +669,5 @@ class UserController
         session_start();
         require_once __DIR__ . './../view/about_us.php';
     }
-    public function try() {
-        session_start();
-        require_once __DIR__ . './../view/try.php';
-    }
 
 }
