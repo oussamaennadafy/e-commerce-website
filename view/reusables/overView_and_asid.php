50f43e0a40a0d0e81a468221cebf907b7e6a8acc
oussama <oussama.ennadafy@gmail.com>
2022-06-16 16:57:28 +0100

    update

---
+++ view/reusables/overView_and_asid.php
@@ -0,0 +1,107 @@
+<section class="bg-gray-100 py-10">
+		<div class="container max-w-screen-xl mx-auto px-4">
+
+			<div class="flex flex-wrap -mx-2">
+				<div class="lg:w-3/4 px-2">
+
+					<article class="border border-gray-200 shadow-sm rounded bg-white">
+						<nav class="border-b px-4">
+							<a href="#" class="px-3 py-4 mx-1 inline-block border-b border-blue-600 text-blue-600 hover:border-blue-600 hover:text-blue-500">Overview</a>
+							<a href="#" class="px-3 py-4 mx-1 inline-block hover:border-blue-600 hover:text-blue-500">Specification</a>
+							<a href="#" class="px-3 py-4 mx-1 inline-block hover:border-blue-600 hover:text-blue-500">Delivery</a>
+							<a href="#" class="px-3 py-4 mx-1 inline-block hover:border-blue-600 hover:text-blue-500">Payment info</a>
+							<a href="#" class="px-3 py-4 mx-1 inline-block hover:border-blue-600 hover:text-blue-500">Seller profile</a>
+						</nav>
+						<div class="p-5 text-gray-700">
+							<p class="mb-3">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+							<p class="mb-3">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. <br> Excepteur sint occaecat cupidatat non
+								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+							<p class="mb-3">
+								Dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. <br> Excepteur sint occaecat cupidatat non
+								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+							<p class="mb-3">
+								Consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor
+							</p>
+						</div>
+					</article>
+				</div> <!-- col.// -->
+				<aside class="lg:w-1/4 px-2">
+
+					<article class="border border-gray-200 shadow-sm rounded bg-white p-4">
+						<h3 class="mb-5 text-lg font-semibold">Similar products</h3>
+
+						<figure class="flex flex-row mb-4">
+							<div>
+								<a href="#" class="block w-20 h-20 rounded border border-gray-200 overflow-hidden">
+									<img src="../../view/images/items/8.jpg" alt="Title">
+								</a>
+							</div>
+							<figcaption class="ml-3">
+								<p><a href="#" class="text-gray-600 hover:text-blue-600">Travel Bag Jeans Blue Color Modern</a></p>
+								<p class="mt-1 font-semibold">$712.00</p>
+							</figcaption>
+						</figure>
+
+						<figure class="flex flex-row mb-4">
+							<div>
+								<a href="#" class="block w-20 h-20 rounded border border-gray-200 overflow-hidden">
+									<img src="../../view/images/items/9.jpg" alt="Title">
+								</a>
+							</div>
+							<figcaption class="ml-3">
+								<p><a href="#" class="text-gray-600 hover:text-blue-600">Apple Watch Series 4 - Space Gray</a></p>
+								<p class="mt-1 font-semibold">$12.99</p>
+							</figcaption>
+						</figure>
+
+						<figure class="flex flex-row mb-4">
+							<div>
+								<a href="#" class="block w-20 h-20 rounded border border-gray-200 overflow-hidden">
+									<img src="../../view/images/items/10.jpg" alt="Title">
+								</a>
+							</div>
+							<figcaption class="ml-3">
+								<p><a href="#" class="text-gray-600 hover:text-blue-600">Apple Watch Series 4 - Space Gray</a></p>
+								<p class="mt-1 font-semibold">$12.99</p>
+							</figcaption>
+						</figure>
+
+						<figure class="flex flex-row mb-4">
+							<div>
+								<a href="#" class="block w-20 h-20 rounded border border-gray-200 overflow-hidden">
+									<img src="../../view/images/items/11.jpg" alt="Title">
+								</a>
+							</div>
+							<figcaption class="ml-3">
+								<p><a href="#" class="text-gray-600 hover:text-blue-600">Apple Watch Series 4 - Space Gray</a></p>
+								<p class="mt-1 font-semibold">$12.99</p>
+							</figcaption>
+						</figure>
+
+					</article>
+
+				</aside> <!-- col.// -->
+			</div> <!-- grid.// -->
+
+		</div> <!-- container.// -->
+	</section>
---
+++ view/reusables/section_recomanded.php
@@ -0,0 +1,74 @@
+	<!-- SECTION-RECOMMENDED -->
+	<section class="pt-10 pb-20 bg-gray-100">
+		<div class="container max-w-screen-xl mx-auto px-4">
+			<h2 class="text-2xl font-semibold mb-8">Recommended products</h2>
+			
+			<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+				<div>
+					<!-- COMPONENT: PRODUCT CARD -->
+					<article class="shadow-sm rounded bg-white border border-gray-200">
+						<a href="#" class="relative block p-1">
+							<img src="../view/images/items/14.jpg" class="mx-auto w-auto" style="height: 250px" height="250" alt="Product title here">
+						</a>
+						<div class="p-4 border-t border-t-gray-200">
+							<h6>
+								<a href="#" class="text-gray-600 hover:text-blue-500">Product name goes here</a>
+							</h6>
+							<p class="text-sm text-gray-400">Sizes: S, M, XL</p> 
+							<span class="font-semibold">$76.00</span>
+						</div>
+					</article>
+					<!-- COMPONENT: PRODUCT CARD //END -->
+				</div>
+				<div>
+					<!-- COMPONENT: PRODUCT CARD -->
+					<article class="shadow-sm rounded bg-white border border-gray-200">
+						<a href="#" class="relative block p-1">
+							<img src="../view/images/items/12.jpg" class="mx-auto w-auto" style="height: 250px" height="250" alt="Product title here">
+						</a>
+						<div class="p-4 border-t border-t-gray-200">
+							<h6>
+								<a href="#" class="text-gray-600 hover:text-blue-500">Product name goes here</a>
+							</h6>
+							<p class="text-sm text-gray-400">Sizes: S, M, XL</p> 
+							<span class="font-semibold">$76.00</span>
+						</div>
+					</article>
+					<!-- COMPONENT: PRODUCT CARD //END -->
+				</div>
+				<div>
+					<!-- COMPONENT: PRODUCT CARD -->
+					<article class="shadow-sm rounded bg-white border border-gray-200">
+						<a href="#" class="relative block p-1">
+							<img src="../view/images/items/9.jpg" class="mx-auto w-auto" style="height: 250px" height="250" alt="Product title here">
+						</a>
+						<div class="p-4 border-t border-t-gray-200">
+							<h6>
+								<a href="#" class="text-gray-600 hover:text-blue-500">Product name goes here</a>
+							</h6>
+							<p class="text-sm text-gray-400">Sizes: S, M, XL</p> 
+							<span class="font-semibold">$76.00</span>
+						</div>
+					</article>
+					<!-- COMPONENT: PRODUCT CARD //END -->
+				</div>
+				<div>
+					<!-- COMPONENT: PRODUCT CARD -->
+					<article class="shadow-sm rounded bg-white border border-gray-200">
+						<a href="#" class="relative block p-1">
+							<img src="../view/images/items/8.jpg" class="mx-auto w-auto" style="height: 250px" height="250" alt="Product title here">
+						</a>
+						<div class="p-4 border-t border-t-gray-200">
+							<h6>
+								<a href="#" class="text-gray-600 hover:text-blue-500">Product name goes here</a>
+							</h6>
+							<p class="text-sm text-gray-400">Sizes: S, M, XL</p> 
+							<span class="font-semibold">$76.00</span>
+						</div>
+					</article>
+					<!-- COMPONENT: PRODUCT CARD //END -->
+				</div>
+			</div> <!-- grid .// -->
+		</div>
+	</section>
+	<!--  SECTION-RECOMMENDED  //END -->
